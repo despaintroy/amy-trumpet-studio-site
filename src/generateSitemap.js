7fc9c9c25ceb6cb4d43dd6107961b28d0a8aaec7
Troy DeSpain <despaintroy@gmail.com>
2024-07-27 10:48:59 -0600

    sitemap and robots

---
+++ src/generateSitemap.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const URLs = ["http://www.amysmusicstudio.troydespain.com"];
+const DATE = new Date().toISOString();
+
+const contents = [
+  '<?xml version="1.0" encoding="UTF-8"?>',
+  '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+  ...URLs.map(
+    (url) => `<url><loc>${url}</loc><lastmod>${DATE}</lastmod></url>`
+  ),
+  "</urlset>",
+].join("\n");
+
+fs.writeFileSync("../sitemapx.xml", contents);
